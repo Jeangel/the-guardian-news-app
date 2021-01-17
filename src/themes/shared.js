9ffeafc128b933cf461d3e59a6120bb16f1a69b4
John Angel <usw4763@gmail.com>
2021-01-17 18:29:50 -0500

    Adding new gray

--- src/themes/shared.js
+++ src/themes/shared.js
@@ -2,7 +2,8 @@ export const shared = {
   colors: {
     white: '#FFFFFF',
     black: '#343B3C',
-    gray40: '#CED0D0',
+    gray60: '#CED0D0',
+    gray40: '#9B9B9B',
     primary: '#FF7754',
     primaryLight: '#FFF1ED',
     primary50: '#FF77548C',
