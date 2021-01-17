860355f8369e393fd87d9c4816c265485c5d06e4
John Angel <usw4763@gmail.com>
2021-01-17 18:30:37 -0500

    Updating NewsCard WIP

--- src/components/organisms/NewsCard.js
+++ src/components/organisms/NewsCard.js
@@ -8,7 +8,7 @@ import { NewsReadingTime } from '../molecules/NewsReadingTime';
 import { useTheme } from '../../hooks';
 
 const Container = styled(Surface)`
-  padding: 11px 8px;
+  padding: 11px 11px 11px 8px;
   margin-top: 20px;
   box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.12);
   elevation: 8;
@@ -17,6 +17,8 @@ const Container = styled(Surface)`
 `;
 
 const NewsInformationContainer = styled(View)`
+  width: 60%;
+  padding: 2px 0;
 `;
 
 const ThumbnailContainer = styled(View)`
@@ -25,9 +27,25 @@ const ThumbnailContainer = styled(View)`
 `;
 
 const Thumbnail = styled(Image)`
-  height: 140px;
+  height: 190px;
   width: 100%;
   border-radius: 10px;
+`;
+
+const Title = styled(Text)`
+  width: 90%;
+  padding-bottom: 6px;
+`;
+
+const Description = styled(Text)`
+  width: 100%;
+  height: 58%;
+`;
+
+const DateAndTimeContainer = styled(View)`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
 `;
 
 export const NewsCard = () => {
@@ -36,6 +54,7 @@ export const NewsCard = () => {
     <Container>
       <ThumbnailContainer>
         <Thumbnail
+          resizeMode="cover"
           source={{
             uri:
               'https://media.guim.co.uk/988ee711d2dc75cb62c0a88957fda40ee175fdb0/440_500_1926_1156/500.jpg',
@@ -43,9 +62,17 @@ export const NewsCard = () => {
         />
       </ThumbnailContainer>
       <NewsInformationContainer>
-        {/* <NewsDate date={'Some stuff'} color={colors.text} />
-        <NewsReadingTime wordsAmount={'Some stuff'} color={colors.text} />
-        <Text>Hi dude</Text> */}
+        <Title variant="h4" numberOfLines={3}>
+          Colombia protests against police brutality leave at least 10 dead.
+        </Title>
+        <Description variant="small" numberOfLines={8}>
+          Unrest sparked by police killing of lawyer in Bogotá spreads to
+          Medellín, Cali and Manizales.
+        </Description>
+        <DateAndTimeContainer>
+          <NewsDate date={'Some stuff'} color={colors.text} />
+          <NewsReadingTime wordsAmount={'Some stuff'} color={colors.text} />
+        </DateAndTimeContainer>
       </NewsInformationContainer>
     </Container>
   );
