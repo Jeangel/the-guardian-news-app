7094127edf393c250b1df332d8a423d4f1de0d76
John Angel <usw4763@gmail.com>
2021-01-17 16:32:59 -0500

    Adding basic TextInput component

---
+++ src/components/molecules/TextInput.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { TextInput as RNTextInput } from 'react-native';
+import styled from 'styled-components';
+import { useTheme } from '../../hooks';
+
+const StyledInput = styled(RNTextInput)`
+  padding: 16px;
+  background-color: ${({ theme }) => theme.colors.inputBackground};
+  border-radius: 16px;
+  width: 100%;
+  max-height: 50px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-family: 'DM Sans';
+  font-weight: 300;
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+/**
+ * Wrapper for React Native TextInput.
+ * @param {{
+ *  value: string,
+ *  onChange: Function,
+ *  placeholder?: string,
+ *  placeholderColor?: string
+ * }} props
+ */
+export const TextInput = ({
+  value,
+  onChange,
+  placeholder,
+  placeholderColor,
+  ...rest
+}) => {
+  const { colors } = useTheme();
+  return (
+    <StyledInput
+      value={value}
+      onChangeText={onChange}
+      placeholder={placeholder}
+      placeholderTextColor={colors.inputPlaceholder}
+      {...rest}
+    />
+  );
+};
