8688bebec4d1214096c2c2a2ed034219dda63d0b
John Angel <usw4763@gmail.com>
2021-01-19 20:34:16 -0500

    Renaming wordsAmount to wordsCount

--- src/components/molecules/NewsReadingTime.js
+++ src/components/molecules/NewsReadingTime.js
@@ -22,16 +22,16 @@ const WORD_PER_MINUTE = 300;
 /**
  *
  * @param {{
- *  wordsAmount: number,
+ *  wordsCount: number,
  *  color: string
  * }} props
  */
-export const NewsReadingTime = ({ wordsAmount = 0, color }) => {
+export const NewsReadingTime = ({ wordsCount, color }) => {
   return (
     <Container>
       <Icon name="clock" size={12} color={color} />
       <Time variant="extra-small" color={color}>
-        {Math.ceil(wordsAmount / WORD_PER_MINUTE)} min read
+        {Math.ceil(wordsCount / WORD_PER_MINUTE)} min read
       </Time>
     </Container>
   );
