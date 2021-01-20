b10c9eb6e58482f584b2aecc477fd45fa67f228d
John Angel <usw4763@gmail.com>
2021-01-19 21:46:41 -0500

    Adding dummy news on SearchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -18,12 +18,32 @@ export const SearchScreen = () => {
     </View>
   );
 
+  const dummyNews = [
+    {
+      title:
+        'Colombia protests against police brutality leave at least 10 dead.',
+      description:
+        'Unrest sparked by police killing of lawyer in Bogotá spreads to Medellín, Cali and Manizales.\nUnrest sparked by police killing of lawyer in Bogotá spreads to Medellín, Cali and Manizales.',
+      date: '2020-12-29T18:55:51Z',
+      wordsCount: 800,
+      thumbnailUrl:
+        'https://media.guim.co.uk/988ee711d2dc75cb62c0a88957fda40ee175fdb0/440_500_1926_1156/500.jpg',
+    },
+    {
+      title:
+        'Colombia protests against police brutality leave at least 10 dead.',
+      description:
+        'Unrest sparked by police killing of lawyer in Bogotá spreads to Medellín, Cali and Manizales.\nUnrest sparked by police killing of lawyer in Bogotá spreads to Medellín, Cali and Manizales.',
+      date: '2020-12-29T18:55:51Z',
+      wordsCount: 800,
+      thumbnailUrl:
+        'https://media.guim.co.uk/988ee711d2dc75cb62c0a88957fda40ee175fdb0/440_500_1926_1156/500.jpg',
+    },
+  ];
+
   return (
     <Canvas>
-      <NewsList
-        news={['Hi', 'Dude', 'a', '', '', '', '', '']}
-        header={listHeader}
-      />
+      <NewsList news={dummyNews} header={listHeader} />
     </Canvas>
   );
 };
