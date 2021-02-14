0173d7fb0af31ec4270d5749275da502243dfaa9
John Angel <usw4763@gmail.com>
2021-02-14 10:29:22 -0500

    Reading bookmarks from store

--- src/screens/BookmarksScreen.js
+++ src/screens/BookmarksScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View } from 'react-native';
 import styled from 'styled-components';
+import { useFocusEffect } from '@react-navigation/native';
 import { Text } from '../components/atoms/Text';
 import { Canvas } from '../components/atoms/Canvas';
 import { NewsList } from '../components/organisms/NewsList';
+import { useBookmarkStore } from '../hooks';
 
 const Title = styled(Text)`
   padding-bottom: 20px;
@@ -11,6 +13,16 @@ const Title = styled(Text)`
 
 export const BookmarksScreen = () => {
   const [news, setNews] = useState([]);
+  const { getStoredBookmarks } = useBookmarkStore();
+  const refreshNews = useCallback(() => {
+    const refresh = async () => {
+      const bookmarks = await getStoredBookmarks();
+      setNews(bookmarks);
+    };
+    refresh();
+  }, [getStoredBookmarks]);
+
+  useFocusEffect(refreshNews);
 
   const listHeader = (
     <View>
@@ -19,7 +31,7 @@ export const BookmarksScreen = () => {
   );
 
   return (
-    <Canvas>
+    <Canvas edges={['top']}>
       <NewsList news={news} header={listHeader} />
     </Canvas>
   );
